1a73daf6c707d126068c6958bc0c1f4418fd5cc8
kleber011 <batistakleber9@gmail.com>
2020-01-31 17:24:42 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$('li.men').hover(
+   function(){ $('.main-menu-men').addClass('show-menu-men') },
+   function(){ $('.main-menu-men').removeClass('show-menu-men') }
+)
+
+$('.main-menu-men').hover(
+   function(){ $('.main-menu-men').addClass('show-menu-men') },
+   function(){ $('.main-menu-men').removeClass('show-menu-men') }
+)
